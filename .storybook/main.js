924e4caef27c082f9f55957f5c68ef247b032ffb
肖子立 <xiaozlb@digitalchina.com>
2022-06-17 09:41:21 +0800

    tutorial-simple-component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,13 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/components/**/*.stories.js",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/components/**/*.stories.js"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app"
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5"
   }
-}
+};
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,10 @@
-export default ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
+import PropTypes from "prop-types";
+
+export default function Task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask
+}) {
   return (
     <div className={`list-item ${state}`}>
       <label className="checkbox">
@@ -8,7 +14,12 @@ export default ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
           disabled={true}
           name="checked"
         />
-        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+        <span
+          className="checkbox-custom"
+          onClick={() => onArchiveTask(id)}
+          id={`archiveTask-${id}`}
+          aria-label={`archiveTask-${id}`}
+        />
       </label>
       <div className="title">
         <input
@@ -18,13 +29,29 @@ export default ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
           placeholder="Input title"
         />
       </div>
+
       <div className="actions" onClick={(event) => event.stopPropagation()}>
         {state !== "TASK_ARCHIVED" && (
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
           <a onClick={() => onPinTask(id)}>
-            <span className="icon-star" />
+            <span
+              className={`icon-star`}
+              id={`pinTask-${id}`}
+              aria-label={`pinTask-${id}`}
+            />
           </a>
         )}
       </div>
     </div>
   );
+}
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired
+  }),
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func
 };
